542c45d22b47fae47f5d971caae1d70b99f64211
hope-mbuthye38 <-C>
2021-10-21 09:31:44 +0300

    admin.py content

---
+++ instanry/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Image, Comment,Like
+
+
+# Register your models here.
+
+admin.site.register(Image)
+admin.site.register(Comment)
+admin.site.register(Like)
+
+
+# Register your models here.
